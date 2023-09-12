8bd23715bcf7817b7d53ef0903659c9d383796c3
Hymiside <vorochaev2004@icloud.com>
2023-09-13 00:24:27 +0500

    added migrations for postgres

--- pkg/repository/postgres/postgres.go
+++ pkg/repository/postgres/postgres.go
@@ -6,7 +6,11 @@ import (
 	"fmt"
 
 	"github.com/Hymiside/test-task-hezzl/pkg/models"
+
 	_ "github.com/lib/pq"
+	"github.com/golang-migrate/migrate/v4"
+    _ "github.com/golang-migrate/migrate/v4/database/postgres"
+    _ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 func NewPostgresDB(ctx context.Context, c models.ConfigPostgresRepository) (*sql.DB, error) {
@@ -23,5 +27,16 @@ func NewPostgresDB(ctx context.Context, c models.ConfigPostgresRepository) (*sql
 	if err = dbP.Ping(); err != nil {
 		return nil, fmt.Errorf("connection test error: %w", err)
 	}
+
+	m, err := migrate.New(
+		"file://migrations",
+		fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", c.User, c.Password, c.Host, c.Port, c.Name),)
+	if err != nil {
+		return nil, err
+	}
+	if err := m.Up(); err != nil {
+		return nil, fmt.Errorf("error to make migrations postgres: %w", err)
+	}
+
 	return dbP, nil
 }
